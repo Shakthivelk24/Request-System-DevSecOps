086be4b2e614732c47bb8a34b98770499fff30db
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-08-02 16:42:12 +0530

    fix(client): add fallback API URL for axios baseURL

--- client/context/UserContext.jsx
+++ client/context/UserContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 import axios from "axios";
 import { useEffect } from "react";
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.baseURL = import.meta.env.VITE_API_URL ||"http://localhost:5000";
 
 // Create a context for user data
 export const userDataContext = createContext();
